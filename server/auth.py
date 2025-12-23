a44477f8fd97f1840aa08b1612ca2c0cf53732b0
Noah Hallows <noahhallows@gmail.com>
2025-12-23 22:16:09 +1100

    tweaked spec

--- server/auth.py
+++ server/auth.py
@@ -1,3 +1,15 @@
 # Authentication service
 
+# Login: main function calls function here to hash password and then compare with db, returns true/false
+# and generates jwt
+# Signup: main function calls function here to hash password and store password, username ect in
+# db. Returns true/false on user account creation success and returns jwt
+
+import argon2
+import psycopg2
+
+
+def login(username: str, password: str, cursor: ):
+    if type(username) !=
+    cursor.execute("SELECT password_hash FROM users WHERE username = %s", (username))
 
