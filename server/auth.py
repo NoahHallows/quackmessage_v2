9d9b711b669e985c20cf4b7e987b3c11c564e0ae
Noah Hallows <noahhallows@gmail.com>
2025-12-24 16:48:27 +1100

    some auth stuff

--- server/auth.py
+++ server/auth.py
@@ -8,10 +8,32 @@
 from argon2 import PasswordHasher
 import psycopg2
 import jwt
+from random import randint
+import smtplib, ssl
+import os
+import smtplib, ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# Import protobufs
+import quackmessage_pb2
+import quackmessage_pb2_grpc
+from db_manager import db
+
+ try:
+    email_server = os.environ.get('EMAIL_SERVER')
+    email_port = int(os.environ.get('EMAIL_PORT'))
+    email_username = os.environ.get('EMAIL_USERNAME')
+    email_password = os.environ.get('EMAIL_PASSWORD')
+    if email_server is None or email_port is None or email_password is None or email_username is None:
+        raise NameError
+except NameError:
+    print("Cannot get email settings from environment", file=stderr)
 
 
 # Convert the list of bytes to a single variable
 def db_binary_to_binary(db_binary):
+    print(type(db_binary))
     binary = b''
     for collumn in db_binary:
         for byte in collumn:
@@ -31,45 +53,95 @@ def create_jwt(user_id: str) -> str:
 def verify_jwt(token: str) -> dict:
     return jwt.decode(token, PUBLIC_KEY, algorithms=["RS256"], audience=AudienceName, issuer=AuthServerName)
 
-# Login function
-def loginHelper(username: str, password: str, cursor: psycopg2.extensions.cursor) -> tuple[bool, str]:
-    print("Login helper")
-    try:
-        cursor.execute("SELECT password_hash FROM users WHERE username = %s", (username,))
-        password_hash = db_binary_to_binary(cursor.fetchall())
 
-    except:
-        print("Password not found", file=stderr)
+class AuthServicer(quackmessage_pb2_grpc.AuthService):
 
-    try:
-        ph = PasswordHasher()
-        ph.verify(password, password_hash)
-        print("Passwords match")
-        token = generate_jwt(username)
-        return (True, token)
+    def __init__(self):
+        self.email = None
+        self.email_verified = False
+        self.email_verification_code = None
 
-    except Exception:
-        print("Passwords do not match")
-        return (False, "")
 
-def createAccountHelper(username: str, password: str, email: str, cursor: cursor: psycopg2.extensions.cursor) -> tuple[bool, str]:
-    cur.execute("SELECT 1 FROM users WHERE username = %s;", (request.username,))
-    if cur.fetchone() is None:
-        ph = PasswordHasher()
-        password_hash = ph.hash(password)
+    # Login function
+    def Login(self, request, context):
+        print("Login")
         try:
-            cur.execute("INSERT INTO users (email, username, password_hash, account_creation_date,
-                    messages_sent, messages_received) VALUES (%s, %s, %s, NOW()), %s,
-                    %s)", (email, username, password_hash, 0, 0))
-            print("Done")
-            token = generate_jwt(username)
-            return (True, token)
-        except:
-            print("Error inserting user into database", file=stderr)
-    else:
-        print("User exists", file=stderr)
+            conn = db.getConn()
+            cursor = conn.cursor()
+            cursor.execute("SELECT password_hash FROM users WHERE username = %s", (request.username,))
+            conn.commit()
+            cursor.close()
+            db.putConn(conn)
+            password_hash = db_binary_to_binary(cursor.fetchall())
 
-    return (False, "")
+        except:
+            print("Password not found", file=stderr)
+
+        try:
+            ph = PasswordHasher()
+            ph.verify(request.password, password_hash)
+            print("Passwords match")
+            token = generate_jwt(request.username)
+            return quackmessage_pb2_grpc.LoginResult(success=True, auth_token=token)
+
+        except Exception:
+            print("Passwords do not match")
+            return quackmessage_pb2_grpc.LoginResult(success=False, auth_token="")
+        context.abort(grpc.StatusCode.UNAUTHENTICATED, "Error")
+
+    def VerifyEmail(self, request, context):
+        # Create a secure SSL context
+        context = ssl.create_default_context()
+
+        # Check email isn't already registered
+        conn = db.getConn()
+        cur = conn.cursor()
+        cur.execute("SELECT 1 FROM users WHERE email = %s;", (request.email,))
+        if cur.fetchone() is not None:
+            return quackmessage_pb2_grpc.VerifyEmailSent(emailSent=False)
+
+        with smtplib.SMTP_SSL(email_server, email_port, context=context) as server:
+            server.login(email_username, email_password)
+            self.email_verification_code = randint(100000, 999999)
+            message_text = "Hello, your verification code is: {code}"
+            server.sendmail(email_username, request.email, message_text.format(code=self.email_verification_code))
+            return quackmessage_pb2_grpc.VerifyEmailSent(emailSent=True)
+
+    def CheckCode(self, request, context):
+        if request.code == self.email_verification_code:
+            self.email_verified = True
+            return quackmessage_pb2_grpc.VerificationCodeMatches(verified=True)
+        else:
+            self.email_verified = False
+            return quackmessage_pb2_grpc.VerificationCodeMatches(verified=False)
+
+    def CreateUser(self, request, context):
+        conn = db.getConn()
+        cur = conn.cursor()
+        cur.execute("SELECT 1 FROM users WHERE username = %s;", (request.username,))
+        if cur.fetchone() is None and self.email is not None and self.email_verified == True:
+            ph = PasswordHasher()
+            password_hash = ph.hash(password)
+            try:
+                cur.execute("INSERT INTO users (email, username, password_hash, account_creation_date,
+                        messages_sent, messages_received) VALUES (%s, %s, %s, NOW()), %s,
+                        %s)", (self.email, username, password_hash, 0, 0))
+                conn.commit()
+                cur.close()
+                db.putConn(conn)
+                print("Done")
+                token = generate_jwt(username)
+                return quackmessage_pb2_grpc.CreateUserResult(success=False, auth_token=token)
+            except:
+                print("Error inserting user into database", file=stderr)
+        else:
+            print("User exists or email is unverified", file=stderr)
+
+        conn.commit()
+        cur.close()
+        db.putConn(conn)
+
+        return quackmessage_pb2_grpc.CreateUserResult(success=False, auth_token="")
 
 
 
---
+++ server/quackmessage_pb2.pyi
@@ -0,0 +1,59 @@
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class LoginMessage(_message.Message):
+    __slots__ = ("username", "password")
+    USERNAME_FIELD_NUMBER: _ClassVar[int]
+    PASSWORD_FIELD_NUMBER: _ClassVar[int]
+    username: str
+    password: str
+    def __init__(self, username: _Optional[str] = ..., password: _Optional[str] = ...) -> None: ...
+
+class LoginResult(_message.Message):
+    __slots__ = ("success", "auth_token")
+    SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    AUTH_TOKEN_FIELD_NUMBER: _ClassVar[int]
+    success: bool
+    auth_token: str
+    def __init__(self, success: bool = ..., auth_token: _Optional[str] = ...) -> None: ...
+
+class VerifyEmailMessage(_message.Message):
+    __slots__ = ("email",)
+    EMAIL_FIELD_NUMBER: _ClassVar[int]
+    email: str
+    def __init__(self, email: _Optional[str] = ...) -> None: ...
+
+class VerificationEmailSent(_message.Message):
+    __slots__ = ("emailSent",)
+    EMAILSENT_FIELD_NUMBER: _ClassVar[int]
+    emailSent: bool
+    def __init__(self, emailSent: bool = ...) -> None: ...
+
+class VerificationCodeMessage(_message.Message):
+    __slots__ = ("code",)
+    CODE_FIELD_NUMBER: _ClassVar[int]
+    code: int
+    def __init__(self, code: _Optional[int] = ...) -> None: ...
+
+class VerificationCodeMatches(_message.Message):
+    __slots__ = ("verified",)
+    VERIFIED_FIELD_NUMBER: _ClassVar[int]
+    verified: bool
+    def __init__(self, verified: bool = ...) -> None: ...
+
+class CreateUserMessage(_message.Message):
+    __slots__ = ("username", "password")
+    USERNAME_FIELD_NUMBER: _ClassVar[int]
+    PASSWORD_FIELD_NUMBER: _ClassVar[int]
+    username: str
+    password: str
+    def __init__(self, username: _Optional[str] = ..., password: _Optional[str] = ...) -> None: ...
+
+class CreateUserResult(_message.Message):
+    __slots__ = ("success",)
+    SUCCESS_FIELD_NUMBER: _ClassVar[int]
+    success: bool
+    def __init__(self, success: bool = ...) -> None: ...
