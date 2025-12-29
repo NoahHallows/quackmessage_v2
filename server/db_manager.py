d98c658dc58bc81c0f9b6f71715fc270a9d8f847
Noah Hallows <noah@quackmail.com.au>
2025-12-29 12:24:38 +1100

    gaodg

--- server/db_manager.py
+++ server/db_manager.py
@@ -10,11 +10,12 @@ class DatabaseManager:
         if DB_USER is None or DB_PASSWORD is None:
             print("Unable to get db username or password", file=sys.stderr)
             sys.exit(1)
-        while True:
+        while x in range(0, 5):
             try:
                 self.conn = psycopg2.connect(
                     user=DB_USER, password=DB_PASSWORD,
                     host='db', port='5432', database='quackmessage')
+                break
             except:
                 print("Waiting for db server, sleeping for 2 seconds")
                 sleep(2)
