0c3cf907dffdee6deabc5f3b9f6b0b07df5bd077
Noah Hallows <noah@quackmail.com.au>
2025-12-29 12:13:36 +1100

    added loop to db manager in case database takes a second to start

--- server/db_manager.py
+++ server/db_manager.py
@@ -1,6 +1,7 @@
 import psycopg2
 import os
 import sys
+from time import sleep
 
 class DatabaseManager:
     def __init__(self):
@@ -9,9 +10,15 @@ class DatabaseManager:
         if DB_USER is None or DB_PASSWORD is None:
             print("Unable to get db username or password", file=sys.stderr)
             sys.exit(1)
-        self.conn = psycopg2.connect(
-            user=DB_USER, password=DB_PASSWORD,
-            host='db', port='5432', database='quackmessage')
+        while True:
+            try:
+                self.conn = psycopg2.connect(
+                    user=DB_USER, password=DB_PASSWORD,
+                    host='db', port='5432', database='quackmessage')
+            except:
+                print("Waiting for db server, sleeping for 2 seconds")
+                sleep(2)
+
 
     def getConn(self):
         return self.conn
